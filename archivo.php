78b19263670a8c0a70ab2f9471aa5b7cb26fddec
albertorv <albertorv.07@gmail.com>
2014-02-21 17:05:33 -0600

    HTML y php
    Ajuste de formulario y código php

--- archivo.php
+++ archivo.php
@@ -1,7 +1,10 @@
 <?php 
 	$location = $_SERVER['REQUEST_URI'];
-	$link = mysql_connect('mysql_host', 'root', '12345');
-	print_r($est);
-	echo "Guardado";
-	$est=save($link);
+
+	
+	
+	$conexion = mysql_connect("localhost", "root","12345");
+	mysql_database("estudiantes",$conexion);
+	$q = mysql_query("select * from estudiante",$conexion);
+
  ?>
